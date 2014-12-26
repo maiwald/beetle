0ea7b2906588e933bd1d6685c87e6b306d51421c
Luciano Maiwald <luciano.maiwald@gmail.com>
2014-12-26 05:33:51 +0100

    add basic reporting to TaskRunner

--- lib/beetle_etl/task_runner/task_runner.rb
+++ lib/beetle_etl/task_runner/task_runner.rb
@@ -6,8 +6,7 @@ module BeetleETL
       @tasks = tasks
 
       @queue = Queue.new
-      @completed = Set.new
-      @running = Set.new
+      @results = {}
     end
 
     def run
@@ -18,39 +17,57 @@ module BeetleETL
             run_task_async(task)
           end
 
-          mark_completed(@queue.pop)
+          task, result = @queue.pop
+          mark_completed(task, result)
         end
       end.join
+
+      @results
     end
 
     private
 
     def run_task_async(task)
       Thread.new do
-        task.run
-        @queue.push task.name
+        @queue.push [task.name, task.run]
       end
     end
 
-    def mark_completed(task_name)
-      @running.delete(task_name)
-      @completed << task_name
+    def mark_completed(task_name, result)
+      @results[task_name][:ended_at] = now
+      @results[task_name][:result] = result
     end
 
     def mark_running(task_name)
-      @running << task_name
+      @results[task_name] = { started_at: now }
     end
 
     def runnables
-      resolvables.reject { |r| @running.include? r.name }
+      resolvables.reject { |r| running.include? r.name }
     end
 
     def resolvables
-      @dependency_resolver.resolvables(@completed)
+      @dependency_resolver.resolvables(completed)
+    end
+
+    def running
+      @results.map do |table_name, task_data|
+        table_name unless task_data.has_key? :ended_at
+      end.compact
+    end
+
+    def completed
+      @results.map do |table_name, task_data|
+        table_name if task_data.has_key? :ended_at
+      end.compact
     end
 
     def all_run?
-      @tasks.map(&:name).all? { |name| @completed.include? name }
+      @tasks.map(&:name).to_set == completed.to_set
+    end
+
+    def now
+      Time.now
     end
 
   end
